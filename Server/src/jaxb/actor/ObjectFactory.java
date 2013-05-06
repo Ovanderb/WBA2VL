be6dc13a8dc0d60f83be6ba159b511c3fdc436ba
Oliver Vanderb <oliver@vanderbuerie.de>
2013-05-06 12:08:17 +0200

    created adminclient first file and some other folders

--- Server/src/jaxb/actor/ObjectFactory.java
+++ Server/src/jaxb/actor/ObjectFactory.java
@@ -1,12 +1,12 @@
 //
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.6 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// ï¿½nderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
 // Generiert: 2013.04.30 um 11:50:02 PM CEST 
 //
 
 
-package generated;
+package jaxb.actor;
 
 import javax.xml.bind.annotation.XmlRegistry;
 
